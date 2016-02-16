9ffc300fbc7ea97853177a3801a528c065f7bca0
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-02-16 16:53:05 +0100

    Did some cleaning

--- src/fathersfarm/Main.java
+++ src/fathersfarm/Main.java
@@ -5,8 +5,12 @@ import org.lwjgl.opengl.*;
 import java.awt.Dimension;
 
 
+/**
+ * The main class for the application.
+*/
 public class Main {
 
+    public static final String TITLE = "Fathers Farm";
     public static final int WIDTH = 640;
     public static final int HEIGHT = WIDTH / 16 * 9;
     public static final int SCALE = 2;
@@ -15,6 +19,7 @@ public class Main {
             HEIGHT * SCALE
             );
 
+
     public Main() throws LWJGLException {
         this.initialize();
         this.loop();
@@ -22,10 +27,16 @@ public class Main {
 
 
     private void initialize() throws LWJGLException {
-        Display.setTitle("Game");
-        Display.setDisplayMode(new DisplayMode(DISPLAY_SIZE.width, DISPLAY_SIZE.height));
+        Display.setTitle(TITLE);
+        Display.setDisplayMode(
+                new DisplayMode(
+                    DISPLAY_SIZE.width,
+                    DISPLAY_SIZE.height
+                    )
+                );
         Display.create();
     }
+
 
     private void loop () {
         while (! Display.isCloseRequested()) {
@@ -34,6 +45,7 @@ public class Main {
         }
         System.exit(0);
     }
+
 
     public static void main(String[] args) {
         try {
