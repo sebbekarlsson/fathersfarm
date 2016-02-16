d3a3b598ff56df3ac5b69dd4e54ff0a6f0ff35de
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-02-16 16:59:22 +0100

    Added some comments and updating display in loop

--- src/fathersfarm/Main.java
+++ src/fathersfarm/Main.java
@@ -19,13 +19,20 @@ public class Main {
             HEIGHT * SCALE
             );
 
-
+    
+    /**
+     * Main calls for the application.
+    */
     public Main() throws LWJGLException {
         this.initialize();
         this.loop();
     }
 
 
+    /**
+     * This function is used to initialize the game,
+     * setting up the display etc.
+    */
     private void initialize() throws LWJGLException {
         Display.setTitle(TITLE);
         Display.setDisplayMode(
@@ -37,16 +44,24 @@ public class Main {
         Display.create();
     }
 
-
+    
+    /**
+     * The initial game loop.
+    */
     private void loop () {
-        while (! Display.isCloseRequested()) {
+        while (!Display.isCloseRequested()) {
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+            GL11.glClearColor(255/255f, 255/255f, 255/255f, 1);
 
             Display.update();
         }
         System.exit(0);
     }
 
-
+    
+    /**
+     * Opening endpoint for the application.
+    */
     public static void main(String[] args) {
         try {
             new Main();
